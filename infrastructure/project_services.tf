e66baa8591ba9607f95a937849ef139458a70abf
marcelschliesser <ms@thinq.digital>
2022-06-21 17:36:33 +0200

    enable the google search console api with terraform

--- infrastructure/project_services.tf
+++ infrastructure/project_services.tf
@@ -1,4 +1,9 @@
 resource "google_project_service" "gsc_api" {
   project = var.project_id
+  service = "searchconsole.googleapis.com"
+}
+
+resource "google_project_service" "iam_creds" {
+  project = var.project_id
   service = "iamcredentials.googleapis.com"
 }
