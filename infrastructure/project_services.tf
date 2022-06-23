c7366009fe4b3f65b216acb60f7c040bcbb861ce
marcelschliesser <ms@thinq.digital>
2022-06-23 11:25:31 +0200

    Refactor Terraform Code
    Use a Variable List for enabling necessary GCP APIs

--- infrastructure/project_services.tf
+++ infrastructure/project_services.tf
@@ -1,9 +1,5 @@
-resource "google_project_service" "gsc_api" {
-  project = var.project_id
-  service = "searchconsole.googleapis.com"
-}
-
-resource "google_project_service" "iam_creds" {
-  project = var.project_id
-  service = "iamcredentials.googleapis.com"
+resource "google_project_service" "apis" {
+  for_each                   = toset(var.apis)
+  service                    = each.value
+  disable_dependent_services = true
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,3 +7,13 @@ variable "region" {
   type    = string
   default = "europe-west3"
 }
+
+variable "apis" {
+  description = "List of apis to enable"
+  type        = list(string)
+  default = [
+    "searchconsole.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "cloudresourcemanager.googleapis.com" //needed by terraform
+  ]
+}
